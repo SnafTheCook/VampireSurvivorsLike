b1f10b93469123d93af3f06780057f412ab60ed6
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2025-07-13 18:26:53 +0200

    fix: look rotation uses postion instead of delta

--- Assets/Scripts/Player/PlayerComponents/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerComponents/PlayerInput.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class PlayerInput : MonoBehaviour, IPlayerInput //super simple input for now //TODO: implement InputSystem
+public class PlayerInput : MonoBehaviour, IPlayerInput
 {
-    private static Camera mainCam;
     [SerializeField] private PlayerControlsSO _controlScheme;
     [SerializeField] private LayerMask _rayFloorLayer;
 
@@ -19,41 +18,20 @@ public class PlayerInput : MonoBehaviour, IPlayerInput //super simple input for 
         {
             _controlScheme = new PlayerControlsSO();
         }
-        if (mainCam == null)
-            mainCam = Camera.main;
     }
 
     void Update()
     {
-        /*if (Input.GetKey(_controlScheme.upButton))
-            direction += new Vector3(0, 0, 1);
-        if (Input.GetKey(_controlScheme.downButton))
-            direction += new Vector3(0, 0, -1);
-        if (Input.GetKey(_controlScheme.leftButton))
-            direction += new Vector3(-1, 0, 0);
-        if (Input.GetKey(_controlScheme.rightButton))
-            direction += new Vector3(1, 0, 0);*/
-
         if (_movementDirection != new Vector3(0, 0, 0))
             OnMovementInput?.Invoke(new MovementData(_movementDirection.normalized, true));
 
 
         if (Input.GetKey(_controlScheme.attackButton))
             OnAttackInput?.Invoke(0);
-
-
     }
 
     public Vector3 GetTargetLookPosition()
     {
-        /*if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out RaycastHit raycast, Mathf.Infinity, _rayFloorLayer.value, QueryTriggerInteraction.Ignore))
-        {
-            Vector3 returnVector = new Vector3(raycast.point.x - transform.position.x, 0, raycast.point.z - transform.position.z);
-            return returnVector;
-        }
-        else
-            return new Vector3(0, 0, 0);*/
-
         return _mousePosition;
     }
 
@@ -61,9 +39,10 @@ public class PlayerInput : MonoBehaviour, IPlayerInput //super simple input for 
     {
         _movementDirection = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
-    public void LookInput(InputAction.CallbackContext context) //TODO: mouse delta doesnt feel natural, probably should find a way to switch between raycasts for mouse and delta for gamepads
+    public void LookInput(InputAction.CallbackContext context)
     {
         Vector3 viewportPosition = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
-        _mousePosition = viewportPosition.normalized;
+        Vector3 viewResolutionHalved = new Vector3(Screen.width / 2, 0 ,Screen.height / 2 ); //mouse position starts if left, bottom corner in the new input system
+        _mousePosition = (viewportPosition - viewResolutionHalved).normalized;
     }
 }
